ea665770440455527db67cb2f86f23f9465e32bd
ParkGyuweon <rbdnjs9@gamil.com>
2026-07-12 14:09:24 +0900

    fetch : Spring boot backend server 붙임

---
+++ back_end/src/main/java/io/github/ParkGyuweon/muplay/MuplayApplication.java
@@ -0,0 +1,13 @@
+package io.github.ParkGyuweon.muplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuplayApplication.class, args);
+	}
+
+}
---
+++ back_end/src/test/java/io/github/ParkGyuweon/muplay/MuplayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ParkGyuweon.muplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
